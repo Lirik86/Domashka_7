04c2236822ebaf703857c9f641452efa9ae6835d
Lirik <SobyaninKA@yandex.ru>
2023-03-14 13:46:10 +0500

    Выполнил задание 1

---
+++ zadanie1/Program.cs
@@ -0,0 +1,30 @@
+﻿double[,] myArray(int row, int col)
+{
+    double[,] array = new double[row,col];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = rnd.Next(-9,10);
+            array[i,j] = array[i,j] + rnd.NextDouble();
+        }
+    }
+    return array;
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        System.Console.WriteLine();
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i,j]:F2}\t");
+        }
+    }
+    System.Console.WriteLine();
+}
+
+double[,] matrix = myArray(3,4);
+PrintArray(matrix);
